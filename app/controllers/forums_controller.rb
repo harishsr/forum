fa5d42d680fb405e70b381da9ac9d1fc7e59d0e6
Harish Ramachandran <harishsr.coder@gmail.com>
2015-07-09 16:12:45 -0400

    A3.01: Forums controller

---
+++ app/controllers/forums_controller.rb
@@ -0,0 +1,26 @@
+class ForumsController < ApplicationController
+  
+  # INESCUD
+  
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
