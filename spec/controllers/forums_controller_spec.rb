82f913f534e927aed7b1c9ea2475922e8a8de781
Harish Ramachandran <harishsr.coder@gmail.com>
2015-07-09 19:42:29 -0400

    A3.02: Early controller test

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe ForumsController, type: :controller do
 
+  # let!(:forum) { Forum.create!( ) }
+
+  it "has a working index" do 
+    get :index
+    expect(response).to be_success
+    expect(:get => "forums").to route_to(
+      :controller => "forums",
+      :action => "index"
+    )
+    expect(response).to render_template('index')
+  end
+
+  it "has a new page" do
+    get :new
+    expect(response).to be_success
+    expect(:get => "forums/new").to route_to(
+      :controller => "forums",
+      :action => "new"
+    )
+    expect(response).to render_template('new')
+  end
+
+  
 end
