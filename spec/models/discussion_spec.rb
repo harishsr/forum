918d476eb3f05a67fa65636d85a57913254227ff
Harish Ramachandran <harishsr.coder@gmail.com>
2015-07-16 15:42:56 -0400

    A3.07: Model validation tests, passing

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Discussion, type: :model do
-  describe "validations" do 
-    # pending
-  end 
+
+  before do 
+    @discussion = Discussion.create!(title: "Talk of Rails", 
+                                      description: "This is where you can talk Rails!")
+  end
+
+  it "should validate a correct discussion" do 
+    expect(@discussion).to be_valid
+  end
+
+  it "should require a valid title" do 
+    @discussion.title = ""
+    expect(@discussion).to be_invalid
+
+    @discussion.title = "X"
+    expect(@discussion).to be_invalid      
+  end
+
+  it "should require a valid description" do 
+    @discussion.description = ""
+    expect(@discussion).to be_invalid
+
+    @discussion.description = "too short"
+    expect(@discussion).to be_invalid
+  end
 end
